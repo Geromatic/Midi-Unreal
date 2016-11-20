c9cab37d18e99331247d064bc3636a763d8e0592
Geromatic <scott.bishel@yahoo.com>
2016-11-19 21:58:52 -0800

    [Workaround] Midi playback is more accurate
    It seems there is about a 4% lost in playback speed.  So I had to apply
    a 4% increase in PPQ resoution.

--- Procedural-Midi/MidiAsset/Source/Midi/Classes/Util/MidiUtil.cpp
+++ Procedural-Midi/MidiAsset/Source/Midi/Classes/Util/MidiUtil.cpp
@@ -18,7 +18,8 @@ long MidiUtil::ticksToMs(long ticks, float bpm, int resolution) {
 }
 
 double MidiUtil::msToTicks(long ms, int mpqn, int ppq) {
-	return ((ms * 1000.0) * ppq) / mpqn;
+	// TODO temp workaround to get more accuracte Midi playback
+	return ((ms * 1000.0) * (ppq * 1.041666667)) / mpqn;
 }
 double MidiUtil::msToTicks(long ms, float bpm, int ppq) {
 	return msToTicks(ms, bpmToMpqn(bpm), ppq);
