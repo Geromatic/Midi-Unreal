fcf1f5b899e10fd4889cf0a0289d524e01014d23
Geromatic <scott.bishel@yahoo.com>
2017-10-06 18:04:14 -0800

    changed default processing

--- Procedural-Midi/MidiAsset/Source/Midi/Classes/Util/MidiProcessor.cpp
+++ Procedural-Midi/MidiAsset/Source/Midi/Classes/Util/MidiProcessor.cpp
@@ -121,7 +121,7 @@ void MidiProcessor::update(double deltaTime = -1) {
 		msElapsed = FPlatformTime::ToMilliseconds(now - mLastMs);
 	}
 
-	double ticksElapsed = MidiUtil::msToTicks(msElapsed, mMPQN, mPPQ) * PlaySpeed;
+	double ticksElapsed = ( (msElapsed * 1000.0) * mPPQ) / mMPQN ) * PlaySpeed;//MidiUtil::msToTicks(msElapsed, mMPQN, mPPQ) * PlaySpeed;
 	if (ticksElapsed < 1) {
 		return;
 	}
